f65630971c3c21cf4b378114acadd538d0768af1
Yaroslav Hamuha <yhamuha@gmail.com>
2018-11-14 14:36:03 -0800

    Fix FinalPoNumberController

--- src/main/java/com/usel/app/controller/FinalPoNumberController.java
+++ src/main/java/com/usel/app/controller/FinalPoNumberController.java
@@ -1,5 +1,7 @@
 package com.usel.app.controller;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,10 +12,15 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 import com.usel.app.model.Customer;
+import com.usel.app.model.Job;
+import com.usel.app.model.Purchase;
+import com.usel.app.model.User;
 import com.usel.app.service.CustomerService;
+import com.usel.app.service.FinalPoNumberService;
 import com.usel.app.service.exception.ServiceException;
 
 @RestController
@@ -22,29 +29,21 @@ public class FinalPoNumberController {
 	
 	private final Logger LOG = LoggerFactory.getLogger(UserController.class);
 	
-	@Autowired
-	CustomerService customerService;
-
+	@RequestMapping(method=RequestMethod.POST,consumes="application/json")
+    @ResponseBody
+    public String generateSaveAndReturn(User user_id, Job job_id, Purchase purchase_id_po ) {
+	
+		FinalPoNumberService finalNumberService = null;
+		String response = null;
 		
-	@RequestMapping(method = RequestMethod.POST)
-	@PostMapping
-	public ResponseEntity<Customer> create(@RequestBody Customer customer, UriComponentsBuilder ucBuilder) {
-		LOG.info("creating new customer: {}", customer.getId());
-		Customer createdCustomer;
-		
-		try {
-			if (customerService.exist(customer.getId())){
-			    LOG.info("a customer with name " + customer.getId() + " already exists");
-			    return new ResponseEntity<Customer>(HttpStatus.CONFLICT);
+			try {
+				response = finalNumberService.generateSaveAndReturnFinalPoNumber(user_id, null, null, job_id, null, purchase_id_po);
+			
+			} catch (ServiceException e) {
+				e.printStackTrace();
 			}
-			createdCustomer = customerService.create(customer);
-		} catch (ServiceException e) {
-			LOG.error("a customer with name " + customer.getId() + " create failed", e);
-			return new ResponseEntity<Customer>(HttpStatus.SERVICE_UNAVAILABLE);
-		}
-		
-		HttpHeaders headers = new HttpHeaders();
-		headers.setLocation(ucBuilder.path("/customers/{id}").buildAndExpand(createdCustomer.getId()).toUri());
-        return new ResponseEntity<Customer>(headers, HttpStatus.CREATED);
-	}
+			return response;
+		        
+    }
+        
 }
